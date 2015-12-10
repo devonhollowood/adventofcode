89e397b87d485036d722ad8e5056c82b88cfba23
Devon Hollowood <devonhollowood@gmail.com>
2015-12-09 21:52:35 -0800

    Complete Day 1 Part 2

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,8 +1,11 @@
 main = do
     input <- readFile "input.txt"
-    print $ countParens input
+    print $ findBasement input
 
 countParens = foldr count 0
-    where count '(' n = n+1
-          count ')' n = n-1
-          count _   n = n
+
+findBasement = length . takeWhile (>= 0) . scanl (flip count) 0
+
+count '(' n = n+1
+count ')' n = n-1
+count _   n = n
