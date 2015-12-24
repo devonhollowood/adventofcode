eb2989a4124c6da17d8d78d5f37d34b0188e49db
Devon Hollowood <devonhollowood@gmail.com>
2015-12-23 18:17:05 -0800

    Complete Day 14 Part 2

--- day14/day14.hs
+++ day14/day14.hs
@@ -4,12 +4,34 @@
 import Data.Attoparsec.ByteString.Char8 hiding (take)
 import qualified Data.ByteString as B
 import Control.Applicative
+import Data.List (sortBy, groupBy, maximumBy, transpose)
+import Data.Ord (comparing)
+import Data.Function (on)
+import qualified Data.MultiSet as MS
 
 main :: IO ()
 main = do
     deer <- parseAssert pHerd <$> B.readFile "input.txt"
     let time = 2503
-    print . maximum . map (traveled time) $ deer
+    print . snd . maxOccur $ scores time deer
+
+maxOccur :: Ord a => MS.MultiSet a -> (a, Int)
+maxOccur = maximumBy (comparing snd) . MS.toOccurList
+
+scores :: Time -> [Reindeer] -> MS.MultiSet Reindeer
+scores time deer = MS.fromList . concat . take time . zipManyWith best $ deer_dists
+    where best = map fst . last . groupBy ((==) `on` snd) . sortBy (comparing snd)
+          best :: [(Reindeer, Distance)] -> [Reindeer]
+          deer_dists = map single_dists deer
+          deer_dists :: [[(Reindeer, Distance)]]
+          single_dists d = map ((,) d) $ distances d
+          single_dists :: Reindeer -> [(Reindeer, Distance)]
+
+zipManyWith :: ([a] -> b) -> [[a]] -> [b]
+zipManyWith f = map f . transpose
+
+distances :: Reindeer -> [Distance]
+distances = tail . scanl (+) 0 . gains
 
 traveled :: Time -> Reindeer -> Distance
 traveled t = sum . take t . gains
@@ -34,7 +56,7 @@ data Reindeer = Reindeer {
     speed :: Int,
     flyTime :: Int,
     restTime :: Int
-}
+} deriving (Eq, Show, Ord)
 
 type Distance = Int
 type Time = Int
