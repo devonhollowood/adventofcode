6f037c0b91f031fd2e9e42a286df4d3eedad3eb0
Devon Hollowood <devonhollowood@gmail.com>
2016-12-07 15:47:29 -0800

    Day 7 part 2

--- 2016/day07/src/main.rs
+++ 2016/day07/src/main.rs
@@ -40,19 +40,37 @@ fn interiors(s: &str) -> Delimited {
     Delimited { inner: &s[start..], start_char: '[', end_char: ']' }
 }
 
-#[derive(Debug)]
-struct Abbas<'a> {
+struct SearchWindows<'a>
+{
     current: Option<&'a str>,
     inner: Delimited<'a>,
+    get_window: Box<Fn(&str) -> Option<&str>>,
+    predicate: Box<Fn(&str) -> bool>,
 }
 
-impl<'a> Iterator for Abbas<'a> {
+impl<'a> SearchWindows<'a> {
+    fn new<F, P>(mut delimiters: Delimited<'a>, get: F, pred: P)
+                 -> SearchWindows<'a> where
+        F: 'static + Fn(&str) -> Option<&str>,
+        P: 'static + Fn(&str) -> bool,
+    {
+        SearchWindows {
+            current: delimiters.next(),
+            inner: delimiters,
+            get_window: Box::new(get),
+            predicate: Box::new(pred),
+            }
+    }
+}
+
+impl<'a> Iterator for SearchWindows<'a>
+{
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
         while self.current.is_some() {
-            while let Some(win) = window(self.current.unwrap(), 4) {
+            while let Some(win) = (self.get_window)(self.current.unwrap()) {
                 self.current = Some(advance(self.current.unwrap()));
-                if is_abba(win) {
+                if (self.predicate)(win) {
                     return Some(win);
                 }
             }
@@ -62,14 +80,36 @@ impl<'a> Iterator for Abbas<'a> {
     }
 }
 
-fn abbas(s: &str) -> Abbas {
-    let mut delimiters = exteriors(s);
-    Abbas { current: delimiters.next(), inner: delimiters }
+fn abbas(s: &str) -> SearchWindows {
+    SearchWindows::new(
+        exteriors(s),
+        |st| window(st, 4),
+        is_abba
+    )
 }
 
-fn anti_abbas(s: &str) -> Abbas {
-    let mut delimiters = interiors(s);
-    Abbas { current: delimiters.next(), inner: delimiters }
+fn anti_abbas(s: &str) -> SearchWindows {
+        SearchWindows::new(
+            interiors(s),
+            |st| window(st, 4),
+            is_abba
+        )
+}
+
+fn abas(s: &str) -> SearchWindows {
+    SearchWindows::new(
+        exteriors(s),
+        |st| window(st, 3),
+        is_aba
+    )
+}
+
+fn anti_abas(s: &str) -> SearchWindows {
+    SearchWindows::new(
+        interiors(s),
+        |st| window(st, 3),
+        is_aba
+    )
 }
 
 fn advance(s: &str) -> &str {
@@ -97,12 +137,41 @@ fn is_abba(candidate: &str) -> bool {
     a == d && b == c && a != b
 }
 
+fn is_aba(candidate: &str) -> bool {
+    if candidate.len() != 3 {
+        return false;
+    }
+    let mut iter = candidate.chars();
+    let a = iter.next().unwrap();
+    let b = iter.next().unwrap();
+    let c = iter.next().unwrap();
+    a == c && a != b
+}
+
+fn is_corresponding_bab(aba: &str, candidate: &str) -> bool {
+    if aba.len() != 3 || candidate.len() != 3 {
+        return false;
+    }
+    let mut cand_iter = candidate.chars();
+    let a = cand_iter.next().unwrap();
+    let b = cand_iter.next().unwrap();
+    let c = cand_iter.next().unwrap();
+    let mut aba_iter = aba.chars();
+    aba_iter.next() == Some(b)
+        && aba_iter.next() == Some(a)
+        && aba_iter.next() == Some(b)
+        && a == c
+}
+
 fn supports_tls(candidate: &str) -> bool {
-    //anti_abbas(candidate).next().is_none()
-    //    && abbas(candidate).next().is_some()
-    let fst = abbas(candidate).count();
-    let snd = anti_abbas(candidate).count();
-    fst >= 1 && snd == 0
+    anti_abbas(candidate).next().is_none()
+        && abbas(candidate).next().is_some()
+}
+
+fn supports_ssl(candidate: &str) -> bool {
+    abas(candidate).any(|aba| {
+        anti_abas(candidate).any(|bab| is_corresponding_bab(aba, bab))
+    })
 }
 
 fn parse_args() -> std::io::Result<Box<Read>> {
@@ -135,6 +204,8 @@ fn main() {
         .unwrap_or_else(|err| panic!("Error reading file: {}", err));
     let ips = read_ips(&mut source)
         .unwrap_or_else(|err| panic!("Error reading ips: {}", err));
-    let n_valid_ips = ips.iter().filter(|ip| supports_tls(&ip)).count();
-    println!("# of valid IPs: {}", n_valid_ips);
+    let n_support_tls = ips.iter().filter(|ip| supports_tls(&ip)).count();
+    let n_support_ssl = ips.iter().filter(|ip| supports_ssl(&ip)).count();
+    println!("# of IPs supporting TLS: {}", n_support_tls);
+    println!("# of IPs supporting SSL: {}", n_support_ssl);
 }
