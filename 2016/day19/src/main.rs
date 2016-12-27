1ba09653582165a5d226990ced9ff71c16fdb11a
Devon Hollowood <devonhollowood@gmail.com>
2016-12-27 15:31:17 -0800

    Day 19 part 2

--- 2016/day19/src/main.rs
+++ 2016/day19/src/main.rs
@@ -10,7 +10,7 @@ fn parse_args() -> u64 {
     let num_str = matches.value_of("number").unwrap();
     match num_str.parse() {
         Ok(num) => num,
-        Err(_) => panic!("Invalid number: {}", num_str)
+        Err(_) => panic!("Invalid number: {}", num_str),
     }
 }
 
@@ -19,9 +19,31 @@ fn josephus(num: u64) -> u64 {
     (num ^ prev_power_2) << 1 | 0b1
 }
 
+fn josephus_across(num: u64) -> u64 {
+    if num < 2 {
+        return 1;
+    }
+    let prev_power_3 = {
+        let mut count = 0;
+        let mut a = num - 1;
+        while a != 0 {
+            a /= 3;
+            count += 1
+        }
+        3u64.pow(count - 1)
+    };
+    let rem = num - prev_power_3;
+    if rem <= prev_power_3 {
+        rem
+    } else {
+        prev_power_3 + (rem - prev_power_3) * 2
+    }
+}
+
 fn main() {
     let num = parse_args();
-    println!("Last elf standing: {}", josephus(num));
+    println!("Last elf standing (part 1): {}", josephus(num));
+    println!("Last elf standing (part 2): {}", josephus_across(num));
 }
 
 #[cfg(test)]
@@ -41,5 +63,19 @@ mod tests {
     #[test]
     fn power_of_two() {
         assert_eq!(josephus(8), 1);
+    }
+
+    #[test]
+    fn josephus_across_examples() {
+        assert_eq!(josephus_across(1), 1);
+        assert_eq!(josephus_across(2), 1);
+        assert_eq!(josephus_across(3), 3);
+        assert_eq!(josephus_across(4), 1);
+        assert_eq!(josephus_across(5), 2);
+        assert_eq!(josephus_across(6), 3);
+        assert_eq!(josephus_across(7), 5);
+        assert_eq!(josephus_across(8), 7);
+        assert_eq!(josephus_across(9), 9);
+        assert_eq!(josephus_across(10), 1);
     }
 }
