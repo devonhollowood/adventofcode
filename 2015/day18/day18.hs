a7f036009bb6e05c31b31a090f47b6f58100a99e
Devon Hollowood <devonhollowood@gmail.com>
2017-01-16 23:37:42 -0800

    2015 Day 18 part 1

---
+++ 2015/day18/day18.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+import qualified Data.Array as Array
+import Turtle
+import qualified Text.Megaparsec as Mpc
+import qualified Text.Megaparsec.Text as Mpc
+import qualified Data.Text as Text
+import qualified Data.Maybe as Maybe
+
+main :: IO ()
+main = sh $ do
+  (fname, n_iter) <- options "Day 18" parser
+  contents <- liftIO $ readTextFile fname
+  initial_grid <-
+        case Mpc.parse (parseGrid <* Mpc.eof)
+             (Text.unpack $ format fp fname) contents of
+          Left parse_err -> die $ Text.pack (Mpc.parseErrorPretty parse_err)
+          Right grid -> return grid
+  let final_grid = iterate nextGrid initial_grid !! n_iter
+  let n_on = length . filter isOn . Array.elems $ lights final_grid
+  printf w final_grid
+  printf ("Number of \"on\" lights (Part 1): "%d%"\n") n_on
+
+newtype Grid = Grid {
+  lights :: Array.Array (Int, Int) Light
+  }
+
+instance Show Grid where
+  show grid@Grid{..} =
+    unlines [
+      concat [show $ lights Array.! (r, c) | c <- [0 .. gridWidth grid - 1]]
+      | r <- [0 .. gridHeight grid - 1]
+      ]
+
+data Light = On | Off
+  deriving (Eq)
+
+instance Show Light where
+  show On = "#"
+  show Off = "."
+
+isOn :: Light -> Bool
+isOn light =
+  case light of
+    On -> True
+    Off -> False
+
+gridWidth :: Grid -> Int
+gridWidth = (+1) . fst . snd . Array.bounds . lights
+
+gridHeight :: Grid -> Int
+gridHeight = (+1) . snd . snd . Array.bounds . lights
+
+at :: Grid -> (Int, Int) -> Maybe Light
+Grid{..} `at` pos
+  | Array.inRange (Array.bounds lights) pos = Just $ lights Array.! pos
+  | otherwise = Nothing
+
+nextGrid :: Grid -> Grid
+nextGrid grid@Grid{..} = Grid $ iemap updateLight lights
+  where
+    updateLight pos light =
+      case light of
+        On -> if on_neighbors pos `elem` [2, 3] then On else Off
+        Off -> if on_neighbors pos == 3 then On else Off
+    on_neighbors (r, c) =
+      length
+      . filter isOn
+      . Maybe.mapMaybe (at grid)
+      $ [(r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
+         (r, c - 1),                 (r, c + 1),
+         (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)]
+
+iemap :: Array.Ix i => (i -> e -> e) -> Array.Array i e -> Array.Array i e
+iemap fn arr = Array.array (Array.bounds arr) (map update $ Array.assocs arr)
+  where
+    update (ix, el) = (ix, fn ix el)
+
+parseGrid :: Mpc.Parser Grid
+parseGrid = do
+  rows <- some parseLight `Mpc.sepEndBy1` Mpc.eol
+  let width = length (head rows)
+  let height = length rows
+  return . Grid $ Array.listArray ((0, 0), (width - 1, height - 1)) (concat rows)
+
+parseLight :: Mpc.Parser Light
+parseLight = (Mpc.char '#' *> return On) <|> (Mpc.char '.' *> return Off)
+
+parser = (,)
+  <$> argPath "input" "File from which to read input"
+  <*> (fromIntegral . Maybe.fromMaybe 100
+       <$> optional (optInteger "n-iter" 'n' "Number of iterations to run")
+      )
