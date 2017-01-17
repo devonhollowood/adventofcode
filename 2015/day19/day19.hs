9d5a995e76cc1593d6a3114aaa20ffe443be906a
Devon Hollowood <devonhollowood@gmail.com>
2017-01-17 03:07:21 -0800

    2015 Day 19 part 2 wip

--- 2015/day19/day19.hs
+++ 2015/day19/day19.hs
@@ -8,6 +8,8 @@ import qualified Text.Megaparsec as Mpc
 import qualified Text.Megaparsec.Text as Mpc
 import qualified Data.Text as Text
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
 import qualified Data.Maybe as Maybe
 import qualified Data.Char as Char
 
@@ -20,6 +22,15 @@ main = sh $ do
           Left parse_err -> die $ Text.pack (Mpc.parseErrorPretty parse_err)
           Right grid -> return grid
   printf ("Number of subs (Part 1): "%d%"\n") (Set.size $ performSubs subs str)
+  printf ("Number of steps from `e` to `"%s%"` (Part 2): "%s%"\n") str $
+    case bfs
+         (Set.toList . performSubs subs)
+         (== str)
+         [\try -> Text.length try > Text.length str]
+         "e"
+    of
+      Just steps -> format d (length steps)
+      Nothing -> "No path found!"
 
 performSubs :: Set.Set Substitution -> Text.Text -> Set.Set Text.Text
 performSubs subs text = Set.unions (performSub <$> Set.toList subs)
@@ -33,6 +44,36 @@ performSubs subs text = Set.unions (performSub <$> Set.toList subs)
             )
       & Set.fromList
 
+bfs :: (Ord state, Show state) =>
+  (state -> [state]) -- generate next states
+  -> (state -> Bool) -- determine if answer found
+  -> [(state -> Bool)] -- functions which prune on True
+  -> state -- initial state
+  -> Maybe [state]
+bfs next pred prunes init = reverse <$> go (Map.singleton init []) Seq.empty init
+  where
+    go visited queue current
+      | pred current = Map.lookup current visited
+      | otherwise =
+        let steps_so_far = Maybe.fromJust $ Map.lookup current visited
+            new_states =
+              Map.fromList . map (\st -> (st, st:steps_so_far)) $ next current
+            new_visited = Map.unionWith shorter visited new_states
+            new_queue =
+              queue Seq.><
+              (Seq.fromList
+               . (filter (\st -> (not $ st `Map.member` visited || any ($ st) prunes)))
+               $ Map.keys new_states
+              )
+        in pop new_queue >>= (\(x, xs) -> go new_visited xs x)
+    push elem queue = queue Seq.|> elem
+    pop queue = case Seq.viewl queue of
+      Seq.EmptyL -> Nothing
+      (x Seq.:< xs) -> Just (x, xs)
+    shorter xs ys
+      | length xs <= length ys = xs
+      | otherwise = ys
+
 data Substitution = Substitution {
   subFrom :: Text.Text,
   subTo :: Text.Text
@@ -42,7 +83,7 @@ data Substitution = Substitution {
 parseInput :: Mpc.Parser (Set.Set Substitution, Text.Text)
 parseInput = (,)
   <$> parseSubs
-  <*> (Mpc.space *> fmap Text.pack (many Mpc.anyChar))
+  <*> (Mpc.space *> parseSymbol <* Mpc.space)
 
 parseSubs :: Mpc.Parser (Set.Set Substitution)
 parseSubs = Set.fromList <$> parseSub `Mpc.sepEndBy` Mpc.eol
@@ -52,7 +93,8 @@ parseSubs = Set.fromList <$> parseSub `Mpc.sepEndBy` Mpc.eol
       Substitution
       <$> parseSymbol
       <*> (Mpc.space *> Mpc.string "=>" *> Mpc.space *> parseSymbol)
-    parseSymbol :: Mpc.Parser Text.Text
-    parseSymbol = Text.pack <$> some (Mpc.satisfy (not . Char.isSpace))
+
+parseSymbol :: Mpc.Parser Text.Text
+parseSymbol = Text.pack <$> some (Mpc.satisfy (not . Char.isSpace))
 
 parser = argPath "input" "File from which to read input"
