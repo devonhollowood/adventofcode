17709dfa386c394567742524475381909c5c7473
Devon Hollowood <devonhollowood@gmail.com>
2017-01-23 02:01:18 -0800

    2015 Day 22 part 2

--- 2015/day22/day22.hs
+++ 2015/day22/day22.hs
@@ -5,40 +5,93 @@ import qualified Data.Text as Text
 import Turtle
 import Control.Lens
 import qualified Data.Ord as Ord
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 import qualified Search
 
 main :: IO ()
 main = sh $ do
-  initial_boss <- options "Day 21" parser
+  (initial_boss, mode, verbose) <- options "Day 22" parser
   let initial_player = Character 50 500 0 0
   let initial_state = BattleState initial_player initial_boss [] Nothing
   (total_mana, steps) <-
-    case runSearch initial_state of
+    case runSearch mode initial_state of
       Just (mana, steps) -> return (mana, steps)
       Nothing -> die "Victory is impossible!"
-  printf "Winning strategy:\n"
-  mapM_ (\(cst, st) ->
-           case st ^. justCast of
-             Just sp -> printf ("    "%s%" ("%d%" mana)\n") (name sp) (cost sp)
-             Nothing -> return ()
-        )
-    steps
-  printf ("Total cost: "%d%" mana\n") total_mana
+  if verbose
+    then printVerboseSolution mode initial_state total_mana steps
+    else printSimpleSolution mode total_mana steps
 
-runSearch initial_state = 
+printVerboseSolution :: Difficulty -> BattleState -> Int
+  -> [(Int, BattleState)] -> Shell ()
+printVerboseSolution mode init total_mana steps = do
+  printf ("Winning strategy ("%s%" mode):\n")
+    (if mode == HardMode then "hard" else "easy")
+  when (mode == HardMode) $ do
+    printState init
+    printf "\n"
+  let transitions = zip (modeEffects mode init: map snd steps) (map snd steps)
+  flip mapM_ transitions $ \(old_st, new_st) ->
+    case new_st ^. justCast of
+      Just sp -> do
+        printTurn old_st sp
+      Nothing -> return ()
+  printf ("Total cost: "%d%" mana\n") total_mana
+  where
+    printTurn st sp = do
+      when (mode == HardMode) $ printf ("Hard mode deals 1 damage to player\n")
+      printState st
+      printf ("  -> Player casts "%s%" ("%d%" mana)\n") (name sp) (cost sp)
+      unless (null $ st ^. effects) $
+        printf ("  -> Effects: "%s%"\n")
+        (Text.intercalate ", " . map (Text.pack . show) $ st ^. effects)
+      let halfway = applyEffects . cast sp $ st
+      printState halfway
+      if (halfway ^. boss . hp >= 0) then do
+        printf ("  -> Boss hits player for "%d%" damage\n") (bossDamage halfway)
+        unless (null $ halfway ^. effects) $
+          printf ("  -> Effects: "%s%"\n")
+          (Text.intercalate ", " . map (Text.pack . show) $ halfway ^. effects)
+        printf "\n"
+        else printf "Player wins!\n\n"
+    printState :: BattleState -> Shell ()
+    printState st = do
+      printf ("Player: "%d%" hp, "%d%" mana, "%d%" armor\n")
+        (st ^. player . hp)
+        (st ^. player . mana)
+        (st ^. player . armor)
+      printf ("Boss: "%d%" hp\n")
+        (st ^. boss . hp)
+
+printSimpleSolution mode mana steps = do
+  printf ("Winning strategy ("%s%" mode):\n")
+    (if mode == HardMode then "hard" else "easy")
+  flip mapM_ steps $ \(cst, st) ->
+    case st ^. justCast of
+      Just sp -> do
+        printf ("    "%s%" ("%d%" mana)\n") (name sp) (cost sp)
+      Nothing -> return ()
+  printf ("Total cost: "%d%" mana\n") mana
+
+runSearch :: Difficulty -> BattleState -> Maybe (Int, [(Int, BattleState)])
+runSearch mode initial_state =
   Search.djikstra
-  (\st -> map (\sp -> (cost sp, fullTurn sp st)) (possibleSpells st))
+  (\st -> map (\sp -> (cost sp, fullTurn mode sp st)) (possibleSpells st))
   (\st -> st ^. (boss . hp) <= 0) -- win
   [\st -> st ^. (player . hp) <= 0] -- lose
-  initial_state
+  (modeEffects mode initial_state)
 
-fullTurn :: Spell -> BattleState -> BattleState
-fullTurn sp st =
+fullTurn :: Difficulty -> Spell -> BattleState -> BattleState
+fullTurn mode sp st =
   let halfway = applyEffects . cast sp $ st
   in
     if halfway ^. boss . hp <= 0
     then halfway
-    else applyEffects . bossTurn $ halfway
+    else applyEffects . modeEffects mode . bossTurn $ halfway
+
+modeEffects :: Difficulty -> BattleState -> BattleState
+modeEffects EasyMode st = st
+modeEffects HardMode st = st & player . hp -~ 1
 
 cast :: Spell -> BattleState -> BattleState
 cast sp st = st
@@ -47,7 +100,10 @@ cast sp st = st
   & justCast .~ Just sp
 
 bossTurn :: BattleState -> BattleState
-bossTurn st = st & player . hp -~ (if raw_damage > 1 then raw_damage else 1)
+bossTurn st = st & player . hp -~ bossDamage st
+
+bossDamage :: BattleState -> Int
+bossDamage st = if raw_damage > 1 then raw_damage else 1
   where
     raw_damage = (st ^. boss . damage) - (st ^. player . armor)
 
@@ -109,7 +165,12 @@ possibleSpells :: BattleState -> [Spell]
 possibleSpells st =
   let current_mana = st ^. player . mana
       current_spells = st ^. effects
-  in filter (\sp -> cost sp <= current_mana && not (sp `elem` current_spells)) [
+      available sp =
+        case List.find (== sp) current_spells of
+          Nothing -> True
+          Just (Spell _ _ (EffectEnd _)) -> True
+          Just (_) -> False
+  in filter (\sp -> cost sp <= current_mana && available sp) [
     magicMissile,
     drain,
     shield,
@@ -176,14 +237,19 @@ bracketedEffect ::
   -> (BattleState -> BattleState) -- Start
   -> (BattleState -> BattleState) -- End
   -> Effect
-bracketedEffect turns f inverse =
-  Ongoing 0 f $ Ongoing (turns - 1) id $ EffectEnd inverse
+bracketedEffect turns f inverse
+  | turns < 1 = error "Cannot have zero-length bracketed effect"
+  | turns == 1 = Ongoing 0 f $ EffectEnd inverse
+  | otherwise = Ongoing 0 f $ Ongoing (turns - 2) id $ EffectEnd inverse
 
 ongoingEffect ::
   Int -- Turns
   -> (BattleState -> BattleState) -- status change
   -> Effect
-ongoingEffect turns f = Ongoing (turns - 2) f $ EffectEnd f
+ongoingEffect turns f
+  | turns < 0 = error "Cannot have negative-length ongoing effect"
+  | turns == 0 = EffectEnd id
+  | otherwise = Ongoing (turns - 1) f $ EffectEnd id
 
 immediateEffect :: (BattleState -> BattleState) -> Effect
 immediateEffect f = EffectEnd f
@@ -192,9 +258,15 @@ remainingTurns :: Effect -> Int
 remainingTurns (Ongoing t _ ef) = (t + 1) + remainingTurns ef
 remainingTurns (EffectEnd f) = 0
 
-parser =
-  Character
-  <$> (fromIntegral <$> optInteger "health" 'p' "Boss health")
-  <*> pure 0
-  <*> pure 0
-  <*> (fromIntegral <$> optInteger "damage" 'd' "Boss damage")
+data Difficulty = EasyMode | HardMode deriving (Eq, Show)
+
+parser = (,,)
+  <$> (Character
+       <$> (fromIntegral <$> optInteger "health" 'p' "Boss health")
+       <*> pure 0
+       <*> pure 0
+       <*> (fromIntegral <$> optInteger "damage" 'd' "Boss damage"))
+  <*> ((\s -> if s then HardMode else EasyMode)
+       <$> switch "hard-mode" 'm' "Set difficulty to hard mode")
+  <*> (switch "verbose" 'v' "produce verbose output")
+  
