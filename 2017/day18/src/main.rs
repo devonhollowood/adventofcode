c62410c1b88c94020c994039dcbe05be468d860c
Devon Hollowood <devonhollowood@gmail.com>
2017-12-19 20:42:23 -0800

    2017 day 18 part 2

--- 2017/day18/src/main.rs
+++ 2017/day18/src/main.rs
@@ -4,6 +4,7 @@ extern crate structopt_derive;
 
 use structopt::StructOpt;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
@@ -68,13 +69,14 @@ impl std::str::FromStr for Value {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Operation {
-    Sound(Value),
+    Snd(Value),
     Set(Address, Value),
     Add(Address, Value),
     Mul(Address, Value),
     Mod(Address, Value),
-    Recover(Value),
+    Rcv(Address),
     Jgz(Value, Value),
 }
 
@@ -94,7 +96,7 @@ impl std::str::FromStr for Operation {
         match parts.first() {
             Some(&"snd") => {
                 assert_len(2)?;
-                Ok(Sound(parts[1].parse()?))
+                Ok(Snd(parts[1].parse()?))
             }
             Some(&"set") => {
                 assert_len(3)?;
@@ -114,7 +116,7 @@ impl std::str::FromStr for Operation {
             }
             Some(&"rcv") => {
                 assert_len(2)?;
-                Ok(Recover(parts[1].parse()?))
+                Ok(Rcv(parts[1].chars().next().ok_or(err)?))
             }
             Some(&"jgz") => {
                 assert_len(3)?;
@@ -131,7 +133,7 @@ fn parse(input: &str) -> Result<Vec<Operation>, ParseError> {
 
 fn part1(instructions: &[Operation]) -> Option<Literal> {
     let mut computer = HashMap::new();
-    let mut sounds = Vec::new();
+    let mut snds = Vec::new();
     let mut instr = 0i64;
     let get = |v: Value, cpu: &HashMap<Address, Literal>| match v {
         Value::Literal(l) => l,
@@ -140,7 +142,7 @@ fn part1(instructions: &[Operation]) -> Option<Literal> {
     while 0 <= instr && (instr as usize) < instructions.len() {
         use Operation::*;
         match instructions[instr as usize] {
-            Sound(v) => sounds.push(get(v, &computer)),
+            Snd(v) => snds.push(get(v, &computer)),
             Set(addr, v) => {
                 *computer.entry(addr).or_insert(0) = get(v, &computer)
             }
@@ -153,9 +155,9 @@ fn part1(instructions: &[Operation]) -> Option<Literal> {
             Mod(addr, v) => {
                 *computer.entry(addr).or_insert(0) %= get(v, &computer)
             }
-            Recover(v) => {
-                if get(v, &computer) != 0 {
-                    return sounds.last().cloned();
+            Rcv(v) => {
+                if get(Value::Address(v), &computer) != 0 {
+                    return snds.last().cloned();
                 }
             }
             Jgz(v, jump) => {
@@ -168,6 +170,121 @@ fn part1(instructions: &[Operation]) -> Option<Literal> {
         instr += 1;
     }
     None
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Status {
+    Running,
+    Waiting,
+    Terminated,
+}
+
+#[derive(Debug)]
+struct Computer {
+    id: u8,
+    status: Status,
+    instruction_ptr: usize,
+    registers: HashMap<Address, Literal>,
+    queue: VecDeque<Literal>,
+}
+
+impl Computer {
+    fn new(id: u8) -> Computer {
+        Computer {
+            id: id,
+            status: Status::Running,
+            instruction_ptr: 0,
+            registers: std::iter::once(('p', Literal::from(id))).collect(),
+            queue: VecDeque::new(),
+        }
+    }
+    fn lookup(&self, val: Value) -> Literal {
+        match val {
+            Value::Literal(l) => l,
+            Value::Address(a) => *self.registers.get(&a).unwrap_or(&0),
+        }
+    }
+    // returns Some(send) when sending a value
+    fn perform(&mut self, op: Operation) -> Option<Literal> {
+        //println!("{:?}", self);
+        assert_eq!(self.status, Status::Running);
+        use Operation::*;
+        let mut ret = None;
+        let mut incr = true;
+        match op {
+            Snd(v) => {
+                let send = self.lookup(v);
+                ret = Some(send)
+            }
+            Set(addr, v) => {
+                *self.registers.entry(addr).or_insert(0) = self.lookup(v);
+            }
+            Add(addr, v) => {
+                *self.registers.entry(addr).or_insert(0) += self.lookup(v);
+            }
+            Mul(addr, v) => {
+                *self.registers.entry(addr).or_insert(0) *= self.lookup(v);
+            }
+            Mod(addr, v) => {
+                *self.registers.entry(addr).or_insert(0) %= self.lookup(v)
+            }
+            Rcv(addr) => match self.queue.pop_front() {
+                Some(lit) => *self.registers.entry(addr).or_insert(0) = lit,
+                None => {
+                    self.status = Status::Waiting;
+                    incr = false;
+                }
+            },
+            Jgz(v, jump) => {
+                if self.lookup(v) > 0 {
+                    let new_pos =
+                        self.instruction_ptr as Literal + self.lookup(jump);
+                    if new_pos < 0 {
+                        self.status = Status::Terminated;
+                    } else {
+                        self.instruction_ptr = new_pos as usize;
+                        incr = false;
+                    }
+                }
+            }
+        }
+        if incr {
+            self.instruction_ptr += 1;
+        }
+        ret
+    }
+}
+
+fn part2(instructions: &[Operation]) -> usize {
+    let mut send_count = 0;
+    let mut computers = [Computer::new(0), Computer::new(1)];
+    let mut current = 0;
+    let mut other = if current == 0 { 1 } else { 0 };
+    loop {
+        let instr = instructions[computers[current].instruction_ptr];
+        if let Some(rcv) = computers[current].perform(instr) {
+            computers[other].queue.push_back(rcv);
+            if current == 1 {
+                send_count += 1;
+            }
+        }
+        if computers[current].instruction_ptr > instructions.len() {
+            computers[current].status = Status::Terminated;
+        }
+        if computers[current].status != Status::Running {
+            std::mem::swap(&mut current, &mut other);
+            match computers[current].status {
+                Status::Running => {}
+                Status::Waiting => if computers[current].queue.is_empty() {
+                    break; // deadlock
+                } else {
+                    computers[current].status = Status::Running; // resume
+                },
+                Status::Terminated => break,
+            }
+        }
+    }
+    send_count
 }
 
 fn main() {
@@ -188,6 +305,7 @@ fn main() {
         "Part 1: {}",
         part1(&instructions).expect("Could not recover a sound!")
     );
+    println!("Part 2: {}", part2(&instructions));
 }
 
 #[derive(StructOpt, Debug)]
@@ -200,20 +318,48 @@ struct Opt {
 mod tests {
     use super::*;
 
-    static INPUT: &str = "set a 1\n\
-                          add a 2\n\
-                          mul a a\n\
-                          mod a 5\n\
-                          snd a\n\
-                          set a 0\n\
-                          rcv a\n\
-                          jgz a -1\n\
-                          set a 1\n\
-                          jgz a -2";
-
     #[test]
     fn part1_test() {
-        let instructions = parse(INPUT).unwrap();
+        let input = "set a 1\n\
+                     add a 2\n\
+                     mul a a\n\
+                     mod a 5\n\
+                     snd a\n\
+                     set a 0\n\
+                     rcv a\n\
+                     jgz a -1\n\
+                     set a 1\n\
+                     jgz a -2";
+        let instructions = parse(input).unwrap();
         assert_eq!(part1(&instructions), Some(4));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = "snd 1\n\
+                     snd 2\n\
+                     snd p\n\
+                     rcv a\n\
+                     rcv b\n\
+                     rcv c\n\
+                     rcv d";
+        let instructions = parse(input).unwrap();
+        assert_eq!(part2(&instructions), 3);
+    }
+
+    #[test]
+    fn part2_test2() {
+        let input = "set a 1\n\
+                     add a 2\n\
+                     mul a a\n\
+                     mod a 5\n\
+                     snd a\n\
+                     set a 0\n\
+                     rcv a\n\
+                     jgz a -1\n\
+                     set a 1\n\
+                     jgz a -2";
+        let instructions = parse(input).unwrap();
+        assert_eq!(part2(&instructions), 1);
     }
 }
