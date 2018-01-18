993e2c243e8abe5412de81f94054672f6c923153
Devon Hollowood <devonhollowood@gmail.com>
2018-01-18 14:16:02 -0800

    2017 day 21 part 01 WIP

---
+++ 2017/day21/src/main.rs
@@ -0,0 +1,282 @@
+extern crate itertools;
+
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use itertools::Itertools;
+use structopt::StructOpt;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum GridBuildError {
+    Unsquare,
+    // ^ Trying to build grid from non-square number of elements
+    BadSideLen,
+    // ^ Side length not divisible by 2 or 3
+}
+
+impl std::fmt::Display for GridBuildError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use GridBuildError::*;
+        match *self {
+            Unsquare => write!(f, "Unsquare"),
+            BadSideLen => write!(f, "Bad Side Length"),
+        }
+    }
+}
+
+impl std::error::Error for GridBuildError {
+    fn description(&self) -> &str {
+        use GridBuildError::*;
+        match *self {
+            Unsquare => "Non-square number of elements",
+            BadSideLen => "Side length not divisible by 2 or 3",
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Rule {
+    before: Vec<bool>,
+    after: Vec<bool>,
+}
+
+type RuleSet = std::collections::HashMap<Grid, Grid>;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Grid {
+    side_len: usize,     // this shall always be divisible by 2 or 3
+    contents: Vec<bool>, // this shall always have side_len * side_len rows
+}
+
+impl Grid {
+    fn empty() -> Grid {
+        Grid {
+            side_len: 0,
+            contents: Vec::new(),
+        }
+    }
+
+    fn from_components(side_len: usize, contents: Vec<bool>) -> Result<Grid, GridBuildError> {
+        if side_len * side_len != contents.len() {
+            return Err(GridBuildError::Unsquare);
+        }
+        if side_len % 2 != 0 && side_len % 3 != 0 {
+            return Err(GridBuildError::BadSideLen);
+        }
+        Ok(Grid {
+            side_len: side_len,
+            contents: contents,
+        })
+    }
+
+    fn from_subgrids<T>(container: T) -> Result<Grid, GridBuildError>
+    where
+        T: IntoIterator<Item = Grid>,
+        T::IntoIter: ExactSizeIterator,
+    {
+        // "sg" = "subgrid"
+        let mut iter = container.into_iter().peekable();
+        let sg_side_len = match iter.peek() {
+            Some(sg) => sg.side_len,
+            None => return Ok(Grid::empty()),
+        };
+        let side_len = ((iter.len() as f64).sqrt() as usize) * sg_side_len;
+        let mut contents = vec![false; side_len * side_len];
+        for (b, sg) in iter.enumerate() {
+            for r in 0..sg_side_len {
+                for c in 0..sg_side_len {
+                    let src_row =
+                        b / (side_len / sg_side_len) * sg_side_len + r;
+                    let src_col =
+                        b % (side_len / sg_side_len) * sg_side_len + c;
+                    contents[src_row * side_len + src_col] =
+                        sg.contents[r * sg_side_len + c];
+                }
+            }
+        }
+        Grid::from_components(side_len, contents)
+    }
+
+    fn from_bools<T>(container: T) -> Result<Grid, GridBuildError>
+    where
+        T: IntoIterator<Item = bool>,
+        T::IntoIter: ExactSizeIterator,
+    {
+        let iter = container.into_iter();
+        let side_len = (iter.len() as f64).sqrt() as usize;
+        Grid::from_components(side_len, iter.collect())
+    }
+
+    fn split(&self) -> Vec<Grid> {
+        // in this function, "sg" = "subgrid"
+        let sg_side_len = if self.contents.len() % 4 == 0 { 2 } else { 3 };
+        let mut grids = Vec::new();
+        for b in 0..self.contents.len() / (sg_side_len * sg_side_len) {
+            let mut sg_contents = vec![false; sg_side_len * sg_side_len];
+            for r in 0..sg_side_len {
+                for c in 0..sg_side_len {
+                    let src_row =
+                        b / (self.side_len / sg_side_len) * sg_side_len + r;
+                    let src_col =
+                        b % (self.side_len / sg_side_len) * sg_side_len + c;
+                    sg_contents[r * sg_side_len + c] =
+                        self.contents[src_row * self.side_len + src_col];
+                }
+            }
+            grids.push(Grid {
+                side_len: sg_side_len,
+                contents: sg_contents,
+            });
+        }
+        grids
+    }
+
+    fn enhance(&self, rules: &RuleSet) -> Grid {
+        let contents = rules
+            .get(self)
+            .cloned()
+            .map(|g| g.contents)
+            .unwrap_or(self.contents.clone());
+        Self::from_bools(contents).expect("Error enhancing grid")
+    }
+
+    fn iterate(&self, rules: &RuleSet) -> Grid {
+        Grid::from_subgrids(self.split().iter().map(|grid| grid.enhance(rules)))
+            .expect("Grid Iteration: Invalid subgrid produced")
+    }
+
+    fn count(&self) -> usize {
+        self.contents.iter().filter(|b| **b).count()
+    }
+
+    fn rotate_ccw(&self) -> Grid {
+        let mut contents = vec![false; self.contents.len()];
+        for r in 0..self.side_len {
+            for c in 0..self.side_len {
+                let dst_row = self.side_len - c - 1;
+                let dst_col = r;
+                contents[dst_row * self.side_len + dst_col] =
+                    self.contents[r * self.side_len + c];
+            }
+        }
+        Grid::from_components(self.side_len, contents).expect("Error rotating")
+    }
+
+    fn flip_lr(&self) -> Grid {
+        let mut contents = vec![false; self.contents.len()];
+        for r in 0..self.side_len {
+            for c in 0..self.side_len {
+                contents[r * self.side_len + (self.side_len - c - 1)] =
+                    self.contents[r * self.side_len + c];
+            }
+        }
+        Grid::from_components(self.side_len, contents).expect("Error flipping")
+    }
+}
+
+impl std::str::FromStr for Grid {
+    type Err = GridBuildError;
+    fn from_str(s: &str) -> Result<Grid, Self::Err> {
+        let contents: Vec<_> = s.bytes()
+            .filter_map(|b| match b {
+                b'#' => Some(true),
+                b'.' => Some(false),
+                _ => None,
+            })
+            .collect();
+        Self::from_bools(contents)
+    }
+}
+
+
+fn parse(input: &str) -> Vec<Rule> {
+    unimplemented!()
+}
+
+fn make_ruleset(rule_list: &[Rule]) -> RuleSet {
+    unimplemented!()
+}
+
+fn start() -> Grid {
+    Grid {
+        side_len: 3,
+        contents: vec![
+            false, true, false, false, false, true, true, true, true
+        ],
+    }
+}
+
+fn part1(rules: &RuleSet) -> usize {
+    itertools::iterate(start(), |g| g.iterate(rules))
+        .nth(6)
+        .unwrap()
+        .count()
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let mut contents = String::new();
+    if opt.input.to_str() == Some("-") {
+        std::io::stdin()
+            .read_to_string(&mut contents)
+            .expect("could not read stdin");
+    } else {
+        let mut file = File::open(&opt.input)
+            .expect(&format!("file {} not found", opt.input.display()));
+        file.read_to_string(&mut contents)
+            .expect(&format!("could not read file {}", opt.input.display()));
+    }
+    let rule_list = parse(&contents);
+    let rules = make_ruleset(&rule_list);
+    println!("Part 1: {}", part1(&rules));
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "day20", about = "Advent of code 2017 day 20")]
+struct Opt {
+    #[structopt(help = "Input file", parse(from_os_str))]
+    input: PathBuf,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn rotate_ccw_test() {
+        assert_eq!(
+            Grid::from_str("#...##...").unwrap().rotate_ccw(),
+            Grid::from_str(".#..#.#..").unwrap(),
+        );
+        assert_eq!(
+            Grid::from_str("#.#.").unwrap().rotate_ccw(),
+            Grid::from_str("..##").unwrap(),
+        );
+        assert_eq!(
+            Grid::from_str("#...##...").unwrap().rotate_ccw().rotate_ccw().rotate_ccw().rotate_ccw(),
+            Grid::from_str("#...##...").unwrap()
+        );
+    }
+
+    #[test]
+    fn flip_lr_test() {
+        assert_eq!(
+            Grid::from_str("#...##...").unwrap().flip_lr(),
+            Grid::from_str("..###....").unwrap(),
+        );
+        assert_eq!(
+            Grid::from_str("#.#.").unwrap().flip_lr(),
+            Grid::from_str(".#.#").unwrap(),
+        );
+        assert_eq!(
+            Grid::from_str("#...##...").unwrap().flip_lr().flip_lr(),
+            Grid::from_str("#...##...").unwrap(),
+        );
+    }
+
+}
