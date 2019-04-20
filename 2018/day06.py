f228c6085b95e49667d1c22c4d569deb5458c4ba
Devon Hollowood <devonhollowood@gmail.com>
2019-04-20 12:32:10 -0700

    2018 day 06 part 1

--- 2018/day06.py
+++ 2018/day06.py
@@ -2,6 +2,7 @@
 """ 2018 AOC Day 6 """
 
 import argparse
+from collections import defaultdict
 import re
 import typing
 import unittest
@@ -12,7 +13,7 @@ Coord = typing.Tuple[int, int]
 
 def parse(puzzle: str) -> typing.List[Coord]:
     ''' Parse the input into a list of (int, int) tuples '''
-    line_re = re.compile(r'\((?P<x>\d+), (?P<y>\d+)\)')
+    line_re = re.compile(r'(?P<x>\d+), (?P<y>\d+)')
     coords = []
     for line in puzzle.splitlines():
         match = line_re.match(line)
@@ -27,11 +28,44 @@ def manhattan_dist(a: Coord, b: Coord) -> int:
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
 
+def iter_border(upper_left: Coord, lower_right: Coord) -> typing.Iterator[Coord]:
+    ''' Iterate over square border defined by the given coordinates, clockwise
+    '''
+    for x in range(upper_left[0], lower_right[0] + 1):
+        yield (x, upper_left[1])
+    for y in range(upper_left[1], lower_right[1] + 1):
+        yield (lower_right[0], y)
+    for x in range(lower_right[0], upper_left[0] - 1, -1):
+        yield (x, lower_right[1])
+    for y in range(lower_right[1], upper_left[1] - 1, -1):
+        yield (upper_left[0], y)
+
+
+def iter_area(upper_left: Coord, lower_right: Coord):
+    ''' Iterate over square area defined by the given coordinates '''
+    for x in range(upper_left[0], lower_right[0] + 1):
+        for y in range(upper_left[1], lower_right[1] + 1):
+            yield (x, y)
+
+
 def part1(puzzle):
     """ Solve part 1 """
     coords = parse(puzzle)
+
+    def closest(start):
+        return min(coords, key=lambda coord: manhattan_dist(start, coord))
+
     upper_left = (min(c[0] for c in coords), min(c[1] for c in coords))
     lower_right = (max(c[0] for c in coords), max(c[1] for c in coords))
+
+    infinite = set(closest(point)
+                   for point in iter_border(upper_left, lower_right))
+    areas = defaultdict(lambda: 0)
+    for point in iter_area(upper_left, lower_right):
+        nearest = closest(point)
+        if nearest not in infinite:
+            areas[nearest] += 1
+    return max(areas.values())
 
 
 def part2(puzzle):
@@ -56,7 +90,7 @@ if __name__ == '__main__':
 
 class Part1Test(unittest.TestCase):
     def test_example(self):
-        self.assertEq(part1('1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9'), 17)
+        self.assertEqual(part1('1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9'), 17)
 
 
 class Part2Test(unittest.TestCase):
