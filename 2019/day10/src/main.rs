97aedfc045f6b0d21670bd4e363f1251b306f629
Devon Hollowood <devonhollowood@gmail.com>
2019-12-28 11:31:08 -0800

    2019 day 10 part 2

--- 2019/day10/src/main.rs
+++ 2019/day10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -57,6 +57,82 @@ fn part1(asteroids: &HashSet<Position>) -> usize {
         .expect("Expected >0 asteroids")
 }
 
+#[derive(Debug)]
+struct VaporizeOrder {
+    // asteroids, keyed by their clockwise angle from vertical in radians
+    order: VecDeque<VecDeque<Position>>,
+}
+
+impl VaporizeOrder {
+    fn new(origin: Position, asteroids: &HashSet<Position>) -> VaporizeOrder {
+        use itertools::Itertools;
+        // map position into clockwise angle from south. Due south is mapped to 0, due north is
+        // mapped to -pi. Just east of due north is mapped to pi
+        fn make_key(offset: Offset) -> f64 {
+            // Map branch cut. Remember: north is -y direction
+            // transformation: (x, y) -> (-y, x)
+            // also, remember atan2 takes it's arguments as y, x
+            let key = -f64::atan2(offset.0 as f64, offset.1 as f64);
+            assert!(key.is_finite(), "All keys should be finite");
+            key
+        }
+        let dist_to_origin = move |pos: Position| {
+            let offset = pos - origin;
+            (offset.0 as f64).hypot(offset.1 as f64)
+        };
+        let mut asteroids: Vec<Position> = asteroids
+            .iter()
+            .copied()
+            .filter(|pos| *pos != origin)
+            .collect();
+        asteroids.sort_by(|a, b| {
+            make_key(*a - origin)
+                .partial_cmp(&make_key(*b - origin))
+                .expect("Finite keys should be ordered")
+                .then(
+                    dist_to_origin(*a)
+                        .partial_cmp(&dist_to_origin(*b))
+                        .expect("hypot should never return infinite number"),
+                )
+        });
+        VaporizeOrder {
+            order: asteroids
+                .into_iter()
+                .group_by(|pos| make_key(*pos - origin))
+                .into_iter()
+                .map(|(_, group)| group.collect())
+                .collect(),
+        }
+    }
+}
+
+impl Iterator for VaporizeOrder {
+    type Item = Position;
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(mut queue) = self.order.pop_front() {
+            if let Some(next) = queue.pop_front() {
+                if !queue.is_empty() {
+                    self.order.push_back(queue);
+                }
+                return Some(next);
+            }
+        }
+        None
+    }
+}
+
+fn part2(asteroids: &HashSet<Position>) -> isize {
+    let origin = asteroids
+        .iter()
+        .copied()
+        .max_by_key(|pos| visible_from(*pos, &asteroids))
+        .expect("Expected at least one asteroid");
+    let target = VaporizeOrder::new(origin, asteroids)
+        .nth(199)
+        .expect("Not enough asteroids vaporized");
+    target.0 * 100 + target.1
+}
+
 fn parse(input: &str) -> HashSet<Position> {
     input
         .lines()
@@ -78,6 +154,7 @@ fn main() {
     let input =
         parse(&std::fs::read_to_string(Opt::from_args().input).expect("error reading file"));
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[derive(StructOpt)]
@@ -115,10 +192,13 @@ mod tests {
     }
 
     #[test]
-    fn test_example_1() {
+    fn test_part1() {
         let examples = vec![
             (include_str!("../examples/1.txt"), Position(3, 4), 8),
             (include_str!("../examples/2.txt"), Position(5, 8), 33),
+            (include_str!("../examples/3.txt"), Position(1, 2), 35),
+            (include_str!("../examples/4.txt"), Position(6, 3), 41),
+            (include_str!("../examples/5.txt"), Position(11, 13), 210),
         ];
         for (input, best_pos, n_visible) in examples {
             let asteroids = parse(input);
@@ -131,5 +211,25 @@ mod tests {
             );
             assert_eq!(part1(&asteroids), n_visible);
         }
+    }
+
+    #[test]
+    fn test_vaporization() {
+        let asteroids = parse(include_str!("../examples/6.txt"));
+        let actual: Vec<_> = VaporizeOrder::new(Position(8, 3), &asteroids)
+            .map(|Position(x, y)| (x, y))
+            .collect();
+        assert_eq!(&actual[0..5], &[(8, 1), (9, 0), (9, 1), (10, 0), (9, 2)]);
+        // after first rotation
+        assert_eq!(
+            &actual[30..35],
+            &[(8, 0), (10, 1), (14, 0), (16, 1), (13, 3)]
+        );
+    }
+
+    #[test]
+    fn test_part2() {
+        let asteroids = parse(include_str!("../examples/5.txt"));
+        assert_eq!(part2(&asteroids), 802);
     }
 }
