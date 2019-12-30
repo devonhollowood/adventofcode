af1484a09858ca4dc4d68fbb68872a0b2e76c1cd
Devon Hollowood <devonhollowood@gmail.com>
2019-12-29 23:25:20 -0800

    2019 day 12 part 2

--- 2019/day12/src/main.rs
+++ 2019/day12/src/main.rs
@@ -2,11 +2,29 @@ use regex::Regex;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Vec3d {
     x: isize,
     y: isize,
     z: isize,
+}
+
+impl Vec3d {
+    fn get(&self, dim: Dimension) -> isize {
+        match dim {
+            X => self.x,
+            Y => self.y,
+            Z => self.z,
+        }
+    }
+
+    fn get_mut(&mut self, dim: Dimension) -> &mut isize {
+        match dim {
+            X => &mut self.x,
+            Y => &mut self.y,
+            Z => &mut self.z,
+        }
+    }
 }
 
 impl std::ops::Add for &Vec3d {
@@ -20,7 +38,16 @@ impl std::ops::Add for &Vec3d {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Dimension {
+    X,
+    Y,
+    Z,
+}
+
+use Dimension::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Moon {
     position: Vec3d,
     velocity: Vec3d,
@@ -73,6 +100,36 @@ fn update(moons: &[Moon]) -> Vec<Moon> {
     new_moons
 }
 
+fn update_along_dimension(moons: &[Moon], dim: Dimension) -> Vec<Moon> {
+    // update velocity via gravity
+    let mut new_moons = moons.to_owned();
+    for moon in new_moons.iter_mut() {
+        for other in moons {
+            *moon.velocity.get_mut(dim) +=
+                (other.position.get(dim) - moon.position.get(dim)).signum();
+        }
+    }
+    // update position via velocity
+    for moon in new_moons.iter_mut() {
+        *moon.position.get_mut(dim) += moon.velocity.get(dim);
+    }
+    new_moons
+}
+
+fn orbit(moons: &[Moon], dim: Dimension) -> usize {
+    for (count, state) in std::iter::successors(Some(moons.to_owned()), |moons| {
+        Some(update_along_dimension(moons, dim))
+    })
+    .enumerate()
+    .skip(1)
+    {
+        if state == moons {
+            return count;
+        }
+    }
+    unreachable!()
+}
+
 fn part1(input: &[Moon], count: usize) -> isize {
     std::iter::successors(Some(input.to_owned()), |moons| Some(update(moons)))
         .nth(count)
@@ -82,10 +139,19 @@ fn part1(input: &[Moon], count: usize) -> isize {
         .sum()
 }
 
+fn part2(input: &[Moon]) -> usize {
+    use num::integer::Integer;
+    let x = orbit(input, X);
+    let y = orbit(input, Y);
+    let z = orbit(input, Z);
+    x.lcm(&y).lcm(&z)
+}
+
 fn main() {
     let input: Vec<_> =
         parse(&std::fs::read_to_string(Opt::from_args().input).expect("error reading file"));
     println!("Part 1: {}", part1(&input, 1000));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[derive(StructOpt)]
@@ -102,5 +168,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(&parse(include_str!("../examples/1.txt")), 10), 179);
         assert_eq!(part1(&parse(include_str!("../examples/2.txt")), 100), 1940);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(include_str!("../examples/1.txt"))), 2772);
+        assert_eq!(part2(&parse(include_str!("../examples/2.txt"))), 4686774924);
     }
 }
