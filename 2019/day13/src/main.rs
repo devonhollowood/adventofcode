dbb6efa9b9654c6d8b8852ce31c86d907183f881
Devon Hollowood <devonhollowood@gmail.com>
2019-12-31 00:02:26 -0800

    Add gif-making

--- 2019/day13/src/main.rs
+++ 2019/day13/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -50,6 +51,24 @@ impl GameState {
             .0
              .0
     }
+
+    fn width(&self) -> usize {
+        self.board
+            .keys()
+            .map(|pos| usize::try_from(pos.0).expect("expected all-positive xs"))
+            .max()
+            .unwrap()
+            + 1
+    }
+
+    fn height(&self) -> usize {
+        self.board
+            .keys()
+            .map(|pos| usize::try_from(pos.1).expect("expected all-positive ys"))
+            .max()
+            .unwrap()
+            + 1
+    }
 }
 
 fn read_gamestate(state: &[isize]) -> GameState {
@@ -83,6 +102,30 @@ fn part1(tape: &[isize]) -> usize {
         .count()
 }
 
+fn write_frame<W: std::io::Write>(
+    encoder: &mut gif::Encoder<W>,
+    state: &GameState,
+) -> std::io::Result<()> {
+    let mut pixels = Vec::<u8>::new();
+    let mut frame = gif::Frame::default();
+    frame.width = state.width() as u16;
+    frame.height = state.height() as u16;
+    for y in 0..(state.height() as isize) {
+        for x in 0..(state.width() as isize) {
+            let color = match state.board.get(&(x, y)).unwrap_or(&Empty) {
+                Empty => 0,
+                Wall => 1,
+                Block => 2,
+                Paddle => 3,
+                Ball => 4,
+            };
+            pixels.push(color);
+        }
+    }
+    frame.buffer = pixels.into();
+    encoder.write_frame(&frame)
+}
+
 fn part2(tape: &[isize]) -> isize {
     let mut tape = tape.to_owned();
     tape[0] = 2;
@@ -93,10 +136,13 @@ fn part2(tape: &[isize]) -> isize {
         board: HashMap::new(),
         score: None,
     };
+    let mut encoder = None;
+    let mut idx = 0;
     while let Err((err, prog)) = interpreter.clone().run() {
         if err != intcode::ProgramError::NoMoreInput {
             panic!("Unrecoverable error: {:?}", err);
         }
+        // update state
         {
             let update = read_gamestate(&prog.output()[bytes_read..]);
             for (pos, tile) in update.board.iter() {
@@ -104,10 +150,25 @@ fn part2(tape: &[isize]) -> isize {
             }
             state.score = update.score.or(state.score);
         }
-        dbg!(state.board.values().filter(|tile| **tile == Block).count());
+        // record frame
+        write_frame(
+            encoder.get_or_insert_with(|| {
+                gif::Encoder::new(
+                    std::fs::File::create("breakout.gif").unwrap(),
+                    state.width() as u16,
+                    state.height() as u16,
+                    &[0, 0, 0, 255, 128, 128, 255, 0, 255, 255, 0, 0, 0, 255, 0],
+                )
+                .expect("Error creating encoder")
+            }),
+            &state,
+        )
+        .expect("Error writing frame");
+        // check if we've destroyed all blocks
         if state.board.values().filter(|tile| **tile == Block).count() == 0 {
             break;
         }
+        // update AI logic
         let new_ball_pos = state.ball_pos();
         let expected_ball_x = if let Some(last_ball_x) = last_ball_x {
             let ball_next = (
@@ -123,15 +184,13 @@ fn part2(tape: &[isize]) -> isize {
             new_ball_pos.0
         };
         let input = (expected_ball_x - state.paddle_x()).signum();
-        dbg!(last_ball_x);
-        dbg!(new_ball_pos.0);
-        dbg!(expected_ball_x);
-        dbg!(state.paddle_x());
-        dbg!(input);
-        dbg!(state.score);
         bytes_read = prog.output().len();
         last_ball_x = Some(new_ball_pos.0);
         interpreter = prog.with_input(&[input]);
+        idx += 1;
+        if idx >= 5000 {
+            break;
+        }
     }
     state.score.unwrap_or(0)
 }
