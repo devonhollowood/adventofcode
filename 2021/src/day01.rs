c78b68b14ca72f8aef858e0923a0176205236534
Devon Hollowood <devonhollowood@google.com>
2021-12-23 20:36:48 -0800

    2021 day 01

---
+++ 2021/src/day01.rs
@@ -0,0 +1,40 @@
+use anyhow::{Context, Result};
+use itertools::Itertools;
+
+pub fn parse(input: &str) -> Result<Vec<i32>> {
+    input
+        .lines()
+        .map(|line| line.parse().with_context(|| format!("parsing {}", line)))
+        .collect()
+}
+
+pub fn part1(input: &[i32]) -> Result<usize> {
+    Ok(input.iter().tuple_windows().filter(|(a, b)| b > a).count())
+}
+
+pub fn part2(input: &[i32]) -> Result<usize> {
+    Ok(input
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part1(&input).unwrap(), 7);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part2(&input).unwrap(), 5);
+    }
+}
---
+++ 2021/src/main.rs
@@ -0,0 +1,28 @@
+mod day01;
+
+use anyhow::{Context, Result};
+
+macro_rules! aoc {
+    ($i:ident) => {
+        let s = stringify!($i);
+        let n = s
+            .strip_prefix("day")
+            .expect("argument to aoc!() must start with 'day'");
+        let path = format!("input/{}.txt", s);
+        let input = std::fs::read_to_string(&path).with_context(|| {
+            format!(
+                "could not read file '{}'. Please run from the crate root.",
+                &path
+            )
+        })?;
+        let parsed = $i::parse(&input)?;
+        println!("day {} part 1: {}", n, $i::part1(&parsed)?);
+        println!("day {} part 2: {}", n, $i::part2(&parsed)?);
+    };
+}
+
+fn main() -> Result<()> {
+    aoc!(day01);
+
+    Ok(())
+}
